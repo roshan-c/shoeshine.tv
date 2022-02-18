a8b3fc78d2228c9d3f42e92b81cf5e4520a0f359
Roshan Christison <65707039+roshan-c@users.noreply.github.com>
2022-02-18 10:40:20 +0000

    Update outplayed-aio-downloader.py

--- outplayed-aio-downloader.py
+++ outplayed-aio-downloader.py
@@ -3,23 +3,26 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Firefox()
-videokey = input("Please enter the unique 5 character key from your Outplayed.tv link: ")
+videokey = input(
+    "Please enter the unique 5 character key from your Outplayed.tv link: "
+)
 videourl = "https://outplayed.tv/media/" + videokey
 time.sleep(1)
-print (videourl)
+print(videourl)
 
 
 driver.get("https://outplayed.tv/media/35N3y")
 
 assert "outplayed" in driver.title
 
-videolink = driver.find_element_by_name('og:video')
+videolink = driver.find_element_by_name("og:video")
 print(videolink)
 
 import urllib.request
+
 url = videolink
 name = input("Enter the name for the video\n")
-name=name+".mp4"
+name = name + ".mp4"
 try:
     print("Downloading starts...\n")
     urllib.request.urlretrieve(url, name)
