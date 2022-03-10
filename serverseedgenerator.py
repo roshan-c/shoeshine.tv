0306f39c17a8b89313789f56dd6d6bd6eece41c3
Roshan Christison <65707039+roshan-c@users.noreply.github.com>
2022-03-10 13:27:58 +0000

    Delete serverseedgenerator.py

--- serverseedgenerator.py
+++
@@ -1,7 +0,0 @@
-from numpy import random
-seed = random.randint(10000,99999)
-print(seed)
-seedsave = open("seed.txt", "a")
-seedsave.write(str(seed))
-seedsave.close()
-print("Server seed succesfully generated")
