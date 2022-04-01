1e46c12768342072ff57c2ce0432d3afe41816c2
Roshan Christison <65707039+roshan-c@users.noreply.github.com>
2022-04-01 10:13:27 +0100

    Add files via upload

---
+++ gui/upload-gui.py
@@ -0,0 +1,56 @@
+import PySimpleGUI as sg
+import socket
+import tqdm
+import os
+
+sg.theme("DarkTeal2")
+layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],[sg.Button("Submit")]]
+
+###Building Window
+window = sg.Window('My File Browser', layout, size=(600,150))
+    
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event=="Exit":
+        break
+    elif event == "Submit":
+        fileselect = (values["-IN-"])
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
+
+# the ip address or hostname of the server, the receiver
+host = "192.168.1.101"
+# the port, let's use 5001
+port = 5001
+
+# the name of file we want to send, make sure it exists
+filename = fileselect
+# get the file size
+filesize = os.path.getsize(filename)
+
+# create the client socket
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+
+# send the filename and filesize
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+# start sending the file
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(filename, "rb") as f:
+    while True:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in 
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
+s.close()
