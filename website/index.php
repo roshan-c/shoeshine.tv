5f8cedd783ce89a2c95547aa0880b3ba3ee7001a
Roshan Christison <65707039+roshan-c@users.noreply.github.com>
2022-04-07 13:02:51 +0100

    Create index.php

---
+++ website/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shoeshine.tv</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Top -->
+    <nav class="flex-div">
+        <div class="nav-left flex-div">
+            <img src="images/menu.png" class="menu-icon">
+            <img src="images/logo.png" class="logo">
+        </div>
+        
+        <div class="nav-middle flex-div">
+            
+            <div class="search-box flex-div">
+                <input type="text" class="search-input" placeholder="Search">
+                <img src="images/search.png" class="search-icon">
+            </div>
+        </div>
+        
+        <div class="nav-right flex-div">
+            <img src="images/upload.png">
+            <img src="images/placeholder-user.jpg" class="user-icon">
+        </div>
+    </nav>
+    
+    <!-- Sidebar--> 
+    <div class="sidebar">
+        <div class="shortcut-links">
+            <a href=""><img src="images/home.png"><p>Home</p></a>
+            <a href=""><img src="images/subscriprion.png"><p>Friends</p></a>
+            <a href=""><img src="images/library.png"><p>Your Videos</p></a>
+            <hr>
+        </div>
+    </div>
+    
+    <!-- Main -->
+    <div class="container">
+        <div class="banner">
+            <img src="images/banner.jpg">
+        </div>
+        <div class="list-container">
+            <div class="vid-list">
+                <img src="images/thumbnail1.png" class="thumbnail">
+                <div class="flex-div">
+                    <img src="images/placeholder-user.jpg">
+                    <div class="vid-info">
+                        <a href="">Placeholder Title Text</a>
+                        <p>Placeholder User Text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="script.js"></script>      
+</body>
+</html>
